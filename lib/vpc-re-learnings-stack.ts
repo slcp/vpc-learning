f844908cd00dd897bde4401f62a6cee2e6177071
Stuart Forrest <stuart.forrest@infinityworks.com>
2023-08-24 11:42:36 +0100

    auto-scaling, load balanced, web app on instances in private subnet across all availability zones. resilient to an availability zone being down.

--- lib/vpc-re-learnings-stack.ts
+++ lib/vpc-re-learnings-stack.ts
@@ -1,16 +1,88 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { AutoScalingGroup } from "aws-cdk-lib/aws-autoscaling";
+import {
+  AmazonLinuxGeneration,
+  AmazonLinuxImage,
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  LaunchTemplate,
+  UserData,
+  Vpc,
+} from "aws-cdk-lib/aws-ec2";
+import {
+  ApplicationLoadBalancer,
+  ApplicationProtocol,
+  ApplicationTargetGroup,
+  ListenerAction,
+} from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { Construct } from "constructs";
 
 export class VpcReLearningsStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    // Creates routing for public and private subnets - route tables/rules via Internet Gateway and NAT Gateway for public/private
+    const vpc1 = new Vpc(this, "VPC1", {
+      // cidr: Defaults to 10.0.0.0/16
+      maxAzs: 99, // Defaults to 3, high number will results in all AZs being used
+      // natGateways: Defaults to 1 (per AZ)
+      // subnetConfiguration: Defaults to 1 private/1 public per AZ evenly splitting the CIDR range
+      flowLogs: { VPC1: {} }, // Defaults none, when set each key defaults to all types of logs to Cloudwatch
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'VpcReLearningsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Target Group to route requests to
+    const targetGroup = new ApplicationTargetGroup(this, "Target Group 1", {
+      vpc: vpc1,
+      protocol: ApplicationProtocol.HTTP,
+      port: 80,
+    });
+
+    // Load balancer
+    // Defaults to one in every availability zone
+    const alb = new ApplicationLoadBalancer(this, "ALB1", {
+      vpc: vpc1,
+      internetFacing: true,
+      // vpcSubnets: Defaults to SubnetSelection.PRIVATE_WITH_NAT, which is what we created above
+    });
+    // Routing rule for incoming load balanced traffic
+    alb.addListener("HTTP Traffic", {
+      protocol: ApplicationProtocol.HTTP,
+      port: 80,
+      defaultAction: ListenerAction.forward([targetGroup]),
+    });
+
+    // Launch Template start up script
+    const userData = UserData.forLinux();
+    userData.addCommands(
+      "yum install -y nginx",
+      "yum update -y",
+      "yum install -y httpd",
+      "systemctl start httpd",
+      "systemctl enable httpd",
+      'echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html'
+    );
+    // Template for instances launched by the auto-scaling group
+    new LaunchTemplate(this, "Launch Template 1", {
+      userData,
+    });
+
+    // Auto Scaling Group to control scaling in and out
+    const autoscalingGroup = new AutoScalingGroup(this, "AutoScaling Group 1", {
+      vpc: vpc1,
+      // vpcSubnets: Defaults to all private subnets
+      instanceType: InstanceType.of(InstanceClass.T2, InstanceSize.MICRO),
+      machineImage: new AmazonLinuxImage({
+        generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
+      }),
+      maxCapacity: 5,
+      minCapacity: 2,
+      desiredCapacity: 3,
+    });
+    autoscalingGroup.scaleOnCpuUtilization("Scale", {
+      targetUtilizationPercent: 80,
+    });
+    autoscalingGroup.attachToApplicationTargetGroup(targetGroup);
   }
 }
